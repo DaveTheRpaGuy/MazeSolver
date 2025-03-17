2824af15cad7eb8ced68bb075642ffe76e1cb5c7
davetherpaguy <dave@davetherpaguy.com>
2025-03-17 13:22:25 -0400

    I drew a green and red X yay!

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from window import *
+from line import *
+from point import *
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(5, 5), Point(50, 50)), "red")
+    win.draw_line(Line(Point(5, 50), Point(50, 5)), "green")
     win.wait_for_close()
 
 main()
